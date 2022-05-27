c38b5346f776cb990fedd524ef768e58ef840267
Alexander Volkov <AV-html@yandex.ru>
2022-05-27 12:57:11 +0300

    test git collaboration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from './components/Todolist';
+import {v1} from 'uuid';
+
+export type TechType = Array<Tech>
+export interface Tech {
+  id: string,
+  title: string
+  isDone: boolean
+}
+
 
 function App() {
+  const [techList, setTechList] = useState<TechType>([
+    {id: v1(), title: 'React', isDone: true},
+    {id: v1(), title: 'Redux', isDone: true},
+    {id: v1(), title: 'C#', isDone: false},
+    {id: v1(), title: 'Java', isDone: false},
+    {id: v1(), title: 'TS', isDone: true},
+    {id: v1(), title: 'HTML', isDone: true},
+    {id: v1(), title: 'CSS', isDone: true},
+    {id: v1(), title: 'SCSS', isDone: true},
+    {id: v1(), title: 'Python', isDone: false}
+  ])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title={'Tech'} techList={techList}/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Todolist.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {TechType} from '../App';
+
+type PropsType = {
+    title: string
+    techList: TechType
+}
+
+export function Todolist(props: PropsType) {
+    
+    const techListArray = props.techList.map((t) => {
+        return (
+            <li key={t.id}>
+                <button>X</button>
+                <span>{t.title}</span>
+                <input type="checkbox" checked={t.isDone}/>
+
+            </li>
+        )
+    })
+    
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input type="text"/>
+                <button>Add</button>
+            </div>
+            <div>
+                <ul>
+                    {techListArray}
+                </ul>
+            </div>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+}
